3a1fb7becea4b8e8a91a28988767e70ff967fc50
Cypher <jp10010101010000@gmail.com>
2017-08-01 15:13:39 +1000

    Googlified format

--- source/tut1/q1.cpp
+++ source/tut1/q1.cpp
@@ -1,24 +1,23 @@
 #include <iostream>
 
 namespace cs6771 {
-  void order3(int *a, int *b, int *c) {
-    if (*a < *c) {
-      int t = *a;
-      *a = *c;
-      *c = t;
-    }
-    if (*a < *b) {
-      int t = *a;
-      *a = *b;
-      *b = t;
-    }
-    if (*b < *c) {
-      int t = *b;
-      *b = *c;
-      *c = t;
-    }
+void order3(int *a, int *b, int *c) {
+  if (*a < *c) {
+    int t = *a;
+    *a = *c;
+    *c = t;
   }
-
+  if (*a < *b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+  }
+  if (*b < *c) {
+    int t = *b;
+    *b = *c;
+    *c = t;
+  }
+}
 }
 
 int main() {
--- source/tut1/q3.cpp
+++ source/tut1/q3.cpp
@@ -2,41 +2,41 @@
 
 // TODO: Move namespace to a header file
 namespace cs6771 {
-  void order3(int *a, int *b, int *c) {
-    if (*a < *c) {
-      int t = *a;
-      *a = *c;
-      *c = t;
-    }
-    if (*a < *b) {
-      int t = *a;
-      *a = *b;
-      *b = t;
-    }
-    if (*b < *c) {
-      int t = *b;
-      *b = *c;
-      *c = t;
-    }
+void order3(int *a, int *b, int *c) {
+  if (*a < *c) {
+    int t = *a;
+    *a = *c;
+    *c = t;
   }
+  if (*a < *b) {
+    int t = *a;
+    *a = *b;
+    *b = t;
+  }
+  if (*b < *c) {
+    int t = *b;
+    *b = *c;
+    *c = t;
+  }
+}
 
-  void order3(unsigned int& a, unsigned int& b, unsigned int& c) {
-    if (a > c) {
-      int t = a;
-      a = c;
-      c = t;
-    }
-    if (b > c) {
-      int t = b;
-      b = c;
-      c = t;
-    }
-    if (a > b) {
-      int t = a;
-      a = b;
-      b = t;
-    }
+void order3(unsigned int& a, unsigned int& b, unsigned int& c) {
+  if (a > c) {
+    int t = a;
+    a = c;
+    c = t;
   }
+  if (b > c) {
+    int t = b;
+    b = c;
+    c = t;
+  }
+  if (a > b) {
+    int t = a;
+    a = b;
+    b = t;
+  }
+}
 }
 
 int main() {
