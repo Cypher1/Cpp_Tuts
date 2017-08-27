28a2d0920768bc452e7ba2522e6afcc9966a9605
matts1 <mattstark75@gmail.com>
2017-08-27 21:50:21 +1000

    Nearly finished sample solution

--- source/lec6/mon/ass1.cpp
+++ source/lec6/mon/ass1.cpp
@@ -1,34 +1,121 @@
 #include <algorithm>
+#include <cmath>
 #include <fstream>
+#include <functional>
 #include <iomanip>
 #include <iostream>
 #include <iterator>
+#include <map>
 #include <string>
 #include <vector>
 
-using command_it = std::vector<std::string>::const_iterator;
-// note: global variables are bad, global constants are not
-constexpr auto n_repeat = 3U;
-constexpr auto n_reverse = 5U;
+class Num {
+public:
+  Num(int i): isDouble(false), iVal(i) {}
+  Num(double d): isDouble(true), dVal(d) {}
 
-void runcommand(const std::string& s) {
-  if (s == "reverse") {
-    std::vector<int> stack{0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-    std::reverse(stack.rbegin(), stack.rbegin() + n_reverse);
-    std::copy(stack.begin(), stack.end(), std::ostream_iterator<int>(std::cout, " "));
-    std::cout << '\n';
-  } else {
-    std::cout << s << '\n';
+  friend Num operator+(const Num& lhs, const Num& rhs) {
+    if (lhs.isDouble || rhs.isDouble)
+      return Num{lhs.as_double() + rhs.as_double()};
+    else
+      return Num{lhs.as_int() + rhs.as_int()};
   }
+
+  friend Num operator-(const Num& lhs, const Num& rhs) {
+    if (lhs.isDouble || rhs.isDouble)
+      return Num{lhs.as_double() - rhs.as_double()};
+    else
+      return Num{lhs.as_int() - rhs.as_int()};
+  }
+
+  friend Num operator*(const Num& lhs, const Num& rhs) {
+    if (lhs.isDouble || rhs.isDouble)
+      return Num{lhs.as_double() * rhs.as_double()};
+    else
+      return Num{lhs.as_int() * rhs.as_int()};
+  }
+
+  friend Num operator/(const Num& lhs, const Num& rhs) {
+    if (lhs.isDouble || rhs.isDouble)
+      return Num{lhs.as_double() / rhs.as_double()};
+    else
+      return Num{lhs.as_int() / rhs.as_int()};
+  }
+
+  Num sqrt() {
+    if (isDouble)
+      return Num{isDouble};
+    else
+      return Num{static_cast<int>(std::sqrt(iVal))};
+  }
+
+  friend std::ostream& operator<<(std::ostream& stream, const Num& num) {
+    if (num.isDouble)
+      stream << num.dVal;
+    else
+      stream << num.iVal;
+    return stream;
+  }
+
+  double as_double() const {
+    if (isDouble)
+      return dVal;
+    else
+      return static_cast<double>(iVal);
+  }
+
+  int as_int() const {
+    return iVal;
+  }
+
+private:
+  // If using C++17, use std::variant<int, double> instead
+  bool isDouble;
+  int iVal;
+  double dVal;
+};
+
+using command_it = std::vector<std::string>::const_iterator;
+
+Num top_pop(std::vector<Num>& stack) {
+  auto result = std::move(stack.back());
+  stack.pop_back();
+  return result;
 }
 
-command_it runcommands(command_it start, command_it end, unsigned times = 1) {
+void runcommand(std::vector<Num>& stack, const std::string& command) {
+  const auto binaryFunction = [&] (const std::function<Num(Num, Num)>& f, char c) {
+    return [&] () {
+      auto lhs = top_pop(stack);
+      auto rhs = top_pop(stack);
+      stack.push_back(f(lhs, rhs));
+      std::cout << lhs << ' ' << c << ' ' << rhs << " = " << stack.back() << '\n';
+    };
+  };
+
+  const std::map<std::string, std::function<void()> commands{
+      {"add", binaryFunction(std::plus<Num, Num>, '+')},
+      {"sub", binaryFunction(std::minus<Num, Num>, '+')},
+      {"mult", binaryFunction(std::multiplies<Num, Num>, '+')},
+      {"div", binaryFunction(std::divides<Num, Num>, '+')},
+      {"reverse", [&] () { std::reverse(stack.rbegin(), stack.rbegin() + top_pop(stack).as_int()); }},
+  };
+
+  if (commands.find(command) != commands.end())
+    commands.at(command)();
+  else if (command.find(".") == std::string::npos)
+    stack.push_back(Num{std::stoi(command)});
+  else
+    stack.push_back(Num{std::stod(command)});
+}
+
+command_it runcommands(command_it start, command_it end, std::vector<Num>& stack, unsigned times = 1) {
   for (auto i = 0U; i < times; ++i) {
     for (auto upto = start; upto != end; ++upto) {
       if (*upto == "repeat")
-        upto = runcommands(upto + 1, std::find(start, end, "endrepeat"), n_repeat);
+        upto = runcommands(upto + 1, std::find(start, end, "endrepeat"), stack, top_pop(stack).as_int());
       else
-        runcommand(*upto);
+        runcommand(stack, *upto);
     }
   }
   return end;
@@ -40,9 +127,12 @@ int main() {
 
   std::string s;
   std::vector<std::string> commands;
+  std::vector<Num> stack;
+
   while (in >> s)
     // question for you guys: what's the difference between emplace_back and push_back
     commands.emplace_back(std::move(s)); // note the std::move
   
-  runcommands(commands.cbegin(), commands.cend());
+  runcommands(commands.cbegin(), commands.cend(), stack);
 }
+
