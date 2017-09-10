faa809cc82452348c5feb5185ca99acc6e9e0405
matts1 <mattstark75@gmail.com>
2017-09-10 23:12:29 +1000

    Fixed up traitanditerator.cpp

--- source/lec8/traitAndIterator.cpp
+++ source/lec8/traitAndIterator.cpp
@@ -1,6 +1,9 @@
 #include <algorithm>
 #include <forward_list>
 #include <iostream>
+#include <iterator>
+#include <list>
+#include <vector>
 
 #include "list.hpp"
 
@@ -15,10 +18,8 @@ last_value(InputIterator  first, InputIterator last) {
 
 template <typename BidirectionalIterator>
 void myreverse(BidirectionalIterator begin, BidirectionalIterator end) {
-  while ((begin != end) && (begin != --end)) {
-    std::swap(*begin, *end);
-    ++begin;
-  }
+  (void)begin;
+  (void)end;
 }
 
 int main() {
@@ -28,7 +29,14 @@ int main() {
   l.push_back(3);
 
   std::forward_list<int> fl;
+  std::vector<int> vec;
+  std::list<int> ls;
+  myreverse(fl.begin(), fl.end());
+  myreverse(vec.begin(), vec.end());
+  myreverse(ls.begin(), ls.end());
   // std::reverse(fl.begin(), fl.end());
+  std::reverse(vec.begin(), vec.end());
+  std::reverse(ls.begin(), ls.end());
  
   std::cout << last_value(l.begin(), l.end()) << std::endl;
 }
