cffab007ea32e0505c57fe56a69ac52d6f9d8847
Cypher <jp10010101010000@gmail.com>
2017-10-06 08:55:56 +1100

    Library code

---
+++ source/tut8/library.hpp
@@ -0,0 +1,356 @@
+#include <memory>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+// library to store objects of type T and descriptions of type U
+// library<T, U> = Graph<Node = T, Edge = U>
+//
+// forward definition of external iterator class
+
+template<typename T, typename U>
+class LibraryItemIterator;
+
+template<typename T, typename U>
+class ConstLibraryItemIterator;
+
+template <typename T, typename U>
+class Library {
+  public:
+    friend class LibraryItemIterator<T, U>;
+    typedef LibraryItemIterator<T, U> iterator;
+    friend class ConstLibraryItemIterator<T, U>;
+    typedef ConstLibraryItemIterator<T, U> const_iterator;
+    // mutators to change the contents of the library
+    void add(const T& item);
+    unsigned int remove(const T& item);
+    void erase(iterator&& newEnd, iterator&& end);
+    void addRelated(const T& from, const T& to, const U& desc);
+    void printRelated(const T& from);
+    bool inLibrary(const T& item);
+
+    iterator begin();
+    iterator end();
+    const_iterator cbegin() const;
+    const_iterator cend() const;
+
+  // private:
+  public:
+
+    // private inner class to hold objects of type T in the library.
+    class ItemContainer {
+      public:
+
+        // constructor for creating a new ItemContainer to store an item
+        ItemContainer(const T& item) : itemPtr{std::make_shared<T>(item)} {}
+
+        const T& getItem() const { return *itemPtr; }
+        std::shared_ptr<T> getItemPtr() const { return itemPtr; }
+        void addRelated(const ItemContainer& to, const U& desc);
+        void printRelated();
+
+      private:
+        // private inner class to hold information about related works.
+        class RelatedWork {
+          public:
+            RelatedWork(const ItemContainer& link, const U& desc);
+            void printItemAndDescription();
+          private:
+            // private data members of RelatedWork
+            U relatedWorkDescription;
+            std::weak_ptr<T> relatedWorkLink;
+        };
+
+        // private data members of ItemContainer
+        std::shared_ptr<T> itemPtr;
+        std::vector<RelatedWork> relatedWorks;
+    };
+
+    // private data member of Library Class
+    std::vector<ItemContainer> items;
+    auto findItem(const T& search) -> decltype(items.begin()) {
+      return std::find_if(items.begin(), items.end(), [&search] (const ItemContainer& ic) {
+          return search == ic.getItem();
+          });
+    }
+};
+
+template<typename T, typename U>
+class LibraryItemIterator {
+  public:
+    typedef std::ptrdiff_t                     difference_type;
+    typedef std::random_access_iterator_tag    iterator_category;
+    typedef T                                  value_type;
+    typedef std::shared_ptr<T>                 pointer;
+    typedef T&                                 reference;
+
+  private:
+    typedef typename Library<T, U>::ItemContainer container;
+    typedef typename std::vector<container>::iterator  IT;
+    IT curr;
+
+  public:
+    reference operator*() const;
+    pointer operator->() const { return &(operator*()); }
+    LibraryItemIterator& operator++();
+    LibraryItemIterator& operator--();
+    LibraryItemIterator& operator+=(const difference_type&);
+    LibraryItemIterator& operator-=(const difference_type&);
+    LibraryItemIterator operator+(const difference_type& d) const {
+      LibraryItemIterator it{*this};
+      return it+=d;
+    }
+    LibraryItemIterator operator-(const difference_type& d) const {
+      LibraryItemIterator it{*this};
+      return it-=d;
+    }
+    difference_type operator-(const LibraryItemIterator& o) const {
+      return curr - o.curr;
+    }
+
+    bool operator<(const LibraryItemIterator& r) const {
+      return curr < r.curr;
+    }
+
+    IT& getCurr() {
+      return curr;
+    }
+
+    bool operator==(const LibraryItemIterator& other) const;
+    bool operator!=(const LibraryItemIterator& other) const { return !operator==(other); }
+
+    LibraryItemIterator(IT it): curr{it} {}
+};
+
+// ---- LIBRARY ITERATOR FUNCTIONS ----
+
+template<typename T, typename U>
+LibraryItemIterator<T, U> Library<T, U>::begin() {
+  LibraryItemIterator<T, U> beg{items.begin()};
+  return beg;
+}
+
+template<typename T, typename U>
+LibraryItemIterator<T, U> Library<T, U>::end() {
+  LibraryItemIterator<T, U> end{items.end()};
+  return end;
+}
+
+template<typename T, typename U>
+bool LibraryItemIterator<T, U>::operator==(const LibraryItemIterator<T, U>& other) const {
+  return curr == other.curr;
+}
+
+template<typename T, typename U>
+LibraryItemIterator<T, U>& LibraryItemIterator<T, U>::operator++() {
+  curr++;
+  return *this;
+}
+
+template<typename T, typename U>
+LibraryItemIterator<T, U>& LibraryItemIterator<T, U>::operator--() {
+  curr--;
+  return *this;
+}
+
+template<typename T, typename U>
+LibraryItemIterator<T, U>& LibraryItemIterator<T, U>::operator+=(const LibraryItemIterator<T, U>::difference_type& d) {
+  curr += d;
+  return *this;
+}
+
+template<typename T, typename U>
+LibraryItemIterator<T, U>& LibraryItemIterator<T, U>::operator-=(const LibraryItemIterator<T, U>::difference_type& d) {
+  curr -= d;
+  return *this;
+}
+
+template<typename T, typename U>
+typename LibraryItemIterator<T, U>::reference LibraryItemIterator<T, U>::operator*() const {
+  return *curr->getItemPtr();
+}
+
+// ---- LIBRARY ITERATOR FUNCTIONS ----
+
+template<typename T, typename U>
+class ConstLibraryItemIterator {
+  public:
+    typedef std::ptrdiff_t                     difference_type;
+    typedef std::random_access_iterator_tag    iterator_category;
+    typedef const T                            value_type;
+    typedef std::shared_ptr<value_type>        pointer;
+    typedef value_type&                        reference;
+
+  private:
+    typedef typename Library<const T, U>::ItemContainer container;
+    typedef typename std::vector<container>::iterator  IT;
+    IT curr;
+
+  public:
+    reference operator*() const;
+    pointer operator->() const { return &(operator*()); }
+    ConstLibraryItemIterator& operator++();
+    ConstLibraryItemIterator& operator--();
+    ConstLibraryItemIterator& operator+=(const difference_type&);
+    ConstLibraryItemIterator& operator-=(const difference_type&);
+    ConstLibraryItemIterator operator+(const difference_type& d) const {
+      ConstLibraryItemIterator it{*this};
+      return it+=d;
+    }
+    ConstLibraryItemIterator operator-(const difference_type& d) const {
+      ConstLibraryItemIterator it{*this};
+      return it-=d;
+    }
+    difference_type operator-(const ConstLibraryItemIterator& o) const {
+      return curr - o.curr;
+    }
+
+    bool operator<(const ConstLibraryItemIterator& r) const {
+      return curr < r.curr;
+    }
+
+    IT& getCurr() {
+      return curr;
+    }
+
+    bool operator==(const ConstLibraryItemIterator& other) const;
+    bool operator!=(const ConstLibraryItemIterator& other) const { return !operator==(other); }
+
+    ConstLibraryItemIterator(IT it): curr{it} {}
+};
+
+// ---- CONST LIBRARY ITERATOR FUNCTIONS ----
+
+template<typename T, typename U>
+ConstLibraryItemIterator<T, U> Library<T, U>::cbegin() const {
+  ConstLibraryItemIterator<T, U> beg(items.begin());
+  return beg;
+}
+
+template<typename T, typename U>
+ConstLibraryItemIterator<T, U> Library<T, U>::cend() const {
+  ConstLibraryItemIterator<T, U> end(items.end());
+  return end;
+}
+
+template<typename T, typename U>
+bool ConstLibraryItemIterator<T, U>::operator==(const ConstLibraryItemIterator<T, U>& other) const {
+  return getCurr() == other.getCurr();
+}
+
+template<typename T, typename U>
+ConstLibraryItemIterator<T, U>& ConstLibraryItemIterator<T, U>::operator++() {
+  curr++;
+  return *this;
+}
+
+template<typename T, typename U>
+ConstLibraryItemIterator<T, U>& ConstLibraryItemIterator<T, U>::operator--() {
+  curr--;
+  return *this;
+}
+
+template<typename T, typename U>
+ConstLibraryItemIterator<T, U>& ConstLibraryItemIterator<T, U>::operator+=(const ConstLibraryItemIterator<T, U>::difference_type& d) {
+  curr += d;
+  return *this;
+}
+
+template<typename T, typename U>
+ConstLibraryItemIterator<T, U>& ConstLibraryItemIterator<T, U>::operator-=(const ConstLibraryItemIterator<T, U>::difference_type& d) {
+  curr -= d;
+  return *this;
+}
+
+template<typename T, typename U>
+typename ConstLibraryItemIterator<T, U>::reference ConstLibraryItemIterator<T, U>::operator*() const {
+  return curr->getItem();
+}
+
+// ---- CONST LIBRARY ITERATOR FUNCTIONS ----
+
+// method to add a new item to the library.
+template <typename T, typename U>
+void Library<T,U>::add(const T& item) {
+  if(std::find(begin(), end(), item) == end()) {
+    //ItemContainer newItem{item};
+    //items.push_back(newItem);
+    items.emplace_back(item);
+    std::cout << item << " was added to the library\n";
+  } else {
+    std::cout << item << " is already in the library\n";
+  }
+}
+
+// method to add a related works to an existing item
+template <typename T, typename U>
+void Library<T,U>::addRelated(const T& from, const T& to, const U& desc) {
+  // Find the book 'from' which should have this description
+  auto fromIC = findItem(from);
+  if(fromIC == items.end()) return; // fail (we could throw here)
+
+  // Find the book 'to' which is mentioned in the description
+  auto toIC = findItem(to);
+  if(toIC == items.end()) return; // fail (we could throw here)
+
+  // add desc
+  fromIC->addRelated(*toIC, desc);
+}
+
+template <typename T, typename U>
+void Library<T,U>::erase(iterator&& newEnd, iterator&& end) {
+  items.erase(newEnd.getCurr(), end.getCurr());
+}
+// method to remove an item from the library.
+template <typename T, typename U>
+unsigned int Library<T,U>::remove(const T& t) {
+  //items.erase(std::remove_if(items.begin(), items.end(), [&t](const Library<T,U>::ItemContainer& i){ return i.getItem() == t;}), items.end());
+  erase(std::remove_if(begin(), end(), [&t](const T& i){ return i == t;}), end());
+  return static_cast<unsigned int>(items.size());
+}
+
+// method to print the related works for a given item.
+template <typename T, typename U>
+void Library<T,U>::printRelated(const T& from) {
+  // find from itemContainer.
+  auto fromIC = findItem(from);
+  if(fromIC == items.end()) return; // fail or throw
+  fromIC->printRelated();
+}
+
+// method to check if an item is already in the library
+template <typename T, typename U>
+bool Library<T,U>::inLibrary(const T& item) {
+  return findItem(item) != items.end();
+}
+
+// method to add a related works object to the item container.
+template <typename T, typename U>
+void Library<T,U>::ItemContainer::addRelated(const ItemContainer& to, const U& desc) {
+  RelatedWork rw{to,desc};
+  relatedWorks.push_back(rw);
+}
+
+// method to print each related item in a container.
+template <typename T, typename U>
+void Library<T,U>::ItemContainer::printRelated() {
+  for (auto related : relatedWorks) {
+    related.printItemAndDescription();
+  }
+}
+
+// constructor for a related works object
+template <typename T, typename U>
+Library<T,U>::ItemContainer::RelatedWork::RelatedWork(const ItemContainer& link, const U& desc) : relatedWorkDescription{desc} {
+  relatedWorkLink = link.getItemPtr();
+}
+
+// method to print the item and description of a related work.
+template <typename T, typename U>
+void Library<T,U>::ItemContainer::RelatedWork::printItemAndDescription() {
+  if(auto related = relatedWorkLink.lock()) {
+    std::cout << *(related) << " - " << relatedWorkDescription << std::endl;
+  } else {
+    std::cout << "MISSING: " << " - " << relatedWorkDescription << std::endl;
+  }
+}
---
+++ source/tut8/week9.cpp
@@ -0,0 +1,196 @@
+/**
+ * Create a templated value-like library class to store books, movies etc
+ * Each library item has the data and a set of related works
+ * Each related work object has a ptr to the related works item.
+ */
+#include <iostream>
+
+#include "library.hpp"
+
+
+// class to store the description of an item.
+class Description {
+public:
+  Description(const std::string& d) : desc{d} {}
+  friend std::ostream& operator<<(std::ostream&, const Description &);
+private:
+  std::string desc;
+};
+
+// function to print out a description object
+std::ostream& operator<<(std::ostream &os, const Description &d) {
+  os << d.desc;
+  return os;
+}
+
+// class to store a Book
+class Book {
+public:
+  Book(const std::string& b) : name{b} {}
+  friend bool operator==(const Book&, const Book &);
+  friend std::ostream& operator<<(std::ostream&, const Book &);
+private:
+  std::string name;
+};
+
+// method to compare books
+bool operator==(const Book& a, const Book& b) {
+  return a.name == b.name;
+}
+
+// method to print the book details
+std::ostream& operator<<(std::ostream &os, const Book &b) {
+  os << b.name;
+  return os;
+}
+
+class Movie {
+public:
+  Movie(const std::string& _name, const std::string& _director, int _year) : name{_name}, director{_director}, year{_year} {}
+  friend bool operator==(const Movie&, const Movie&);
+  friend bool operator<(const Movie&, const Movie&);
+  friend std::ostream& operator<<(std::ostream&, const Movie&);
+private:
+  std::string name;
+  std::string director;
+  int year;
+};
+
+// method to compare Movies
+bool operator==(const Movie& a, const Movie& b) {
+  return a.name == b.name && a.director == b.director, a.year == b.year;
+}
+
+// method to compare Movies
+bool operator<(const Movie& a, const Movie& b) {
+  return a.name < b.name;
+}
+
+// method to print the book details
+std::ostream& operator<<(std::ostream &os, const Movie &b) {
+  os << b.name << "(" << b.director << ", " << b.year << ")";
+  return os;
+}
+
+void bookMain() {
+  // create a book library
+  Library<Book,Description> bookLibrary;
+
+  // add some book objects
+  Book harryPotter{"Harry Potter"};
+  Book harryPotter1{"Harry Potter"};
+  Book percyJackson{"Percy Jackson"};
+  bookLibrary.add(harryPotter);
+  bookLibrary.add(harryPotter);
+  bookLibrary.add(harryPotter1);
+  bookLibrary.add(percyJackson);
+
+  // create a description and add it between the two books
+  Description d{"Percy Jackson is a similar childrens book that readers of Harry Potter may like"};
+  bookLibrary.addRelated(harryPotter,percyJackson,d);
+
+  // print books related to Harry Potter
+  bookLibrary.printRelated(harryPotter);
+
+  std::cout << std::boolalpha << "confirming that Percy Jackson is in library: " << bookLibrary.inLibrary(percyJackson) << std::endl;
+
+  // remove Percy Jackson
+  std::cout << "number of items in library after removal: " << bookLibrary.remove(percyJackson) << std::endl;
+
+  std::cout << std::boolalpha << "confirming that Percy Jackson is in library: " << bookLibrary.inLibrary(percyJackson) << std::endl;
+
+  // print books related to Harry Potter
+  bookLibrary.printRelated(harryPotter);
+
+  // const auto& bl = bookLibrary;
+
+  // auto it = bl.cbegin();
+  // auto itEnd = bl.cend();
+
+  Library<Book,Description>::iterator nIt = bookLibrary.begin();
+
+
+  std::vector<double> vec{1,2,3,4,5};
+  std::vector<double> vec2{7,3,3,3,5};
+
+  std::fill(vec2.begin(), vec2.end(), 3);
+  /// std::copy(vec.begin(), vec.end()-2, vec2.begin());
+  //
+  std::transform(vec.begin(), vec.end(), vec2.begin(), vec.begin(),
+      [](const double& v1, const double& v2){
+        return v1+v2;
+      });
+
+  std::cout << "[";
+  if(vec.size() != 0) {
+    std::cout << vec[0];
+    std::for_each(vec.begin()+1, vec.end(), [](const double& v) {
+        std::cout << ", " << v;
+    });
+  }
+  std::cout << "]";
+
+
+  for(double& v : vec) {
+    std::cout << v << ", ";
+  }
+  std::cout << "\n";
+
+  std::cout << "TOTAL = " << std::sqrt(std::accumulate(vec.begin(), vec.end(), 0, [](const double& t, const double& v){ return t+v*v;}));
+
+  /*if(it != itEnd) {
+    std::cout << "THERE IS SOMETHING IN THE LIBRARY\n";
+  }*/
+}
+
+void movieMain() {
+  // create a Movie library
+  Library<Movie,Description> movieLibrary;
+
+  // add some Movie objects
+  Movie harryPotter{"Harry Potter", "Chris Columbus", 2001};
+  Movie harryPotter1{"Harry Potter", "Chris Columbus", 2001};
+  Movie percyJackson{"Percy Jackson", "Chris Columbus", 2010};
+  movieLibrary.add(percyJackson);
+  movieLibrary.add(harryPotter);
+  movieLibrary.add(harryPotter);
+  movieLibrary.add(harryPotter1);
+
+  // create a description and add it between the two Movies
+  Description d{"Percy Jackson is a similar childrens Movie that readers of Harry Potter may like"};
+  movieLibrary.addRelated(harryPotter,percyJackson,d);
+
+  std::cout << "UNSORTED\n";
+  for(auto& i : movieLibrary) {
+    std::cout << i << "\n";
+  }
+  std::sort(movieLibrary.begin(), movieLibrary.end());
+
+  std::cout << "SORTED?\n";
+  for(auto& i : movieLibrary) {
+    std::cout << i << "\n";
+  }
+
+  // print Movies related to Harry Potter
+  movieLibrary.printRelated(harryPotter);
+
+  std::cout << std::boolalpha << "confirming that Percy Jackson is in library: " << movieLibrary.inLibrary(percyJackson) << std::endl;
+
+  // remove Percy Jackson
+  std::cout << "number of items in library after removal: " << movieLibrary.remove(percyJackson) << std::endl;
+
+  std::cout << std::boolalpha << "confirming that Percy Jackson is in library: " << movieLibrary.inLibrary(percyJackson) << std::endl;
+
+  // print Movies related to Harry Potter
+  movieLibrary.printRelated(harryPotter);
+
+  for(auto& i : movieLibrary) {
+    std::cout << i << "\n";
+  }
+}
+
+int main() {
+  bookMain();
+  movieMain();
+}
+
